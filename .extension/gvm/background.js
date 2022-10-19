ca3561bde43e074a9de1f0db498f82db21e9fc34
Taj DeHart <taj@greenvision.media>
2022-10-19 09:31:32 -0600

    New file structure, added gulp

--- .extension/gvm/background.js
+++ .extension/gvm/background.js
@@ -1,4 +1,16 @@
-//Open Udesly once the config is downloaded
-  chrome.runtime.onMessage.addListener(() => {
-      chrome.tabs.create({url: "https://udesly.com/app/jamstack"});
+////PUSHERMAN BACKGROUND SCRIPT
+
+//Set configuration defaults
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({
+    domain: '',
+    siteCode: '',
+    staging: true,
+    static: true
   });
+})
+
+//Open Udesly once the config is downloaded
+chrome.runtime.onMessage.addListener(() => {
+  chrome.tabs.create({ url: "https://udesly.com/app/jamstack" });
+});
