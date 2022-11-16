7db3b73ef7271ec159ef4ba6fbd0af844b899eed
Green Vision Media <113368396+greenvisionmedia@users.noreply.github.com>
2022-11-16 13:34:04 -0700

    added magicpaste

--- .extension/gulpfile.js
+++ .extension/gulpfile.js
@@ -30,12 +30,12 @@ function styles() {
 function scripts() {
     return gulp
         .src('./src/*.js')
-        .pipe(terser())
         .pipe(
-            replace('{{modal.html}}', function (s) {
+            replace('{{modal.html}}', () => {
                 return `${fs.readFileSync('./public/modal.min.html', 'utf8')}`;
             })
         )
+        .pipe(terser())
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('./public'));
 }
--- deleteme/index.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcrypt');
-const bundleModule = require('bundle-module');
-
-bundleModule({
-    entry: './myApp/index.js',
-    filename: 'myApp.script.js',
-    env: 'node',
-    output_dir: './dist',
-});
