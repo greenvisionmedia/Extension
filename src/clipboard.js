7ee54c686fe37907ae05b5b688a588b74a9e2322
taj <taj@greenvision.media>
2023-08-27 00:38:32 +0000

    switch away from DOM object

--- src/clipboard.js
+++ src/clipboard.js
@@ -26,7 +26,7 @@ function injectCheckbox(HTMLEmbed) {
     if (!get('svg-checkbox')) {
         // Get the parent element of the checkboxes
         const checkboxDiv = HTMLEmbed.querySelector(
-            ' .bem-Modal_Body > form > div:nth-child(3) > div:first-child'
+            ' .bem-Modal_Body > form > div:nth-child(3) > div:first-child',
         );
 
         // Change a few styles on the existing checkbox wrapper div
@@ -37,16 +37,130 @@ function injectCheckbox(HTMLEmbed) {
         checkboxDiv.insertAdjacentHTML('beforeEnd', '{{clipboard.html}}');
 
         // Register a magic clipboard ui object with both checkboxes and a config method
-        const Clipboard = {
-            SVGCheckbox: get('svg-checkbox'),
-            MDCheckbox: get('md-checkbox'),
-            URICheckbox: get('uri-checkbox'),
-            configure,
-        };
+        const SVGCheckbox = get('svg-checkbox'),
+            MDCheckbox = get('md-checkbox'),
+            URICheckbox = get('uri-checkbox');
+
+        async function configure() {
+            const configData = await chrome.storage.local.get([
+                'SVG_STATE',
+                'MD_STATE',
+                'URI_STATE',
+            ]);
+            if (configData.SVG_STATE) {
+                this.SVGCheckbox.checked = true;
+            }
+            if (configData.MD_STATE) {
+                this.MDCheckbox.checked = true;
+            }
+            if (configData.URI_STATE) {
+                this.URICheckbox.checked = true;
+            }
+        }
+
+        async function modifyClipboard() {
+            const text = await navigator.clipboard.readText(),
+                configData = await chrome.storage.local.get([
+                    'SVG_STATE',
+                    'MD_STATE',
+                    'URI_STATE',
+                ]);
+            if (SVGPragma.test(text)) {
+                // Checking config data to see if SVG compression is on
+                if (configData.SVG_STATE) {
+                    // If so, compress and load that text to the clipboard
+                    const compressedSVG = compressSVG(text);
+                    navigator.clipboard.writeText(compressedSVG);
+                } else if (configData.URI_STATE) {
+                    const convertedSVG = convertSVG(text);
+                    navigator.clipboard.writeText(convertedSVG);
+                }
+            }
+            if (MDPragma.test(text)) {
+                if (configData.MD_STATE) {
+                    const convertedMD = markdown(text.replace(MDPragma, ''));
+                    navigator.clipboard.writeText(convertedMD);
+                }
+            }
+        }
+
+        // This is a severe minification, removes everything but the essential path data including colors, kerning, strokes and stroke effects
+        // Best for small icons or simple shapes (which are the best use-cases for inline SVG in Webflow)
+
+        function convertSVG(svg) {
+            svg = svg.trim();
+            // remove xml, doctype, generator...
+            svg = svg.slice(svg.indexOf('<svg'));
+            // soft validate
+            if (!svg.startsWith('<svg') || !svg.endsWith('svg>')) return;
+            // add namespace if necessary
+            if (!svg.includes('http://www.w3.org/2000/svg'))
+                svg = svg.replace(
+                    /<svg/g,
+                    `<svg xmlns='http://www.w3.org/2000/svg'`,
+                );
+            // remove comments
+            svg = svg.replace(/<!--.{1,}-->/g, '');
+            // remove unnecessary attributes
+            svg = svg.replace(/version=[\"\'](.{0,}?)[\"\'](?=[\s>])/g, '');
+            svg = svg.replace(/id=[\"\'](.{0,}?)[\"\'](?=[\s>])/g, '');
+            svg = svg.replace(/class=[\"\'](.{0,}?)[\"\'](?=[\s>])/g, '');
+            svg = svg.replace(/data-name=[\"\'](.{0,}?)[\"\'](?=[\s>])/g, '');
+            svg = svg.replace(/encoding=[\"\'](.{0,}?)[\"\'](?=[\s>])/g, '');
+            // replace nested quotes
+            svg = svg.replace(/"'(.{1,})'"/g, "'$1'");
+            // replace double quotes
+            svg = svg.replace(/"/g, "'");
+            // remove empty spaces between tags
+            svg = svg.replace(/>\s{1,}</g, '><');
+            // remove duplicate spaces
+            svg = svg.replace(/\s{2,}/g, ' ');
+            // trim again
+            svg = svg.trim();
+            // soft validate again
+            if (!svg.startsWith('<svg') || !svg.endsWith('svg>')) return;
+            // replace ampersand
+            svg = svg.replace(/&/g, '&amp;');
+            // encode only unsafe symbols
+            svg = svg.replace(/[%#<>?\[\\\]^`{|}]/g, encodeURIComponent);
+            // build data uri
+            svg = `data:image/svg+xml,${svg}`;
+            // ok, ship it!
+            return svg;
+        }
+
+        // This is a severe minification, removes everything but the essential path data including colors, kerning, strokes and stroke effects
+        // Best for small icons or simple shapes (which are the best use-cases for inline SVG in Webflow)
+
+        function compressSVG(svg) {
+            svg = svg.trim();
+            // remove xml, doctype, generator...
+            svg = svg.slice(svg.indexOf('<svg'));
+            // soft validate
+            if (!svg.startsWith('<svg') || !svg.endsWith('svg>')) return;
+            // add namespace if necessary
+            if (!svg.includes('http://www.w3.org/2000/svg'))
+                svg = svg.replace(
+                    /<svg/g,
+                    `<svg xmlns='http://www.w3.org/2000/svg'`,
+                );
+            // remove comments
+            svg = svg.replace(/<!--.{1,}-->/g, '');
+            // remove unnecessary attributes
+            svg = svg.replace(/version=[\"\'](.{0,}?)[\"\'](?=[\s>])/g, '');
+            svg = svg.replace(/id=[\"\'](.{0,}?)[\"\'](?=[\s>])/g, '');
+            svg = svg.replace(/class=[\"\'](.{0,}?)[\"\'](?=[\s>])/g, '');
+            svg = svg.replace(/data-name=[\"\'](.{0,}?)[\"\'](?=[\s>])/g, '');
+            svg = svg.replace(/encoding=[\"\'](.{0,}?)[\"\'](?=[\s>])/g, '');
+            svg = svg.replace(/style=[\"\'](.{0,}?)[\"\'](?=[\s>])/g, '');
+            svg = svg.replace(/fill=[\"\'](.{0,}?)[\"\'](?=[\s>])/g, '');
+
+            return svg;
+        }
 
         // Toggles a checkmark, and stores a local variable so these settings are persistant
-        Clipboard.SVGCheckbox.addEventListener('click', () => {
-            if (Clipboard.SVGCheckbox.checked) {
+        SVGCheckbox.addEventListener('click', () => {
+            if (SVGCheckbox.checked) {
                 chrome.storage.local.set({
                     SVG_STATE: true,
                 });
@@ -57,8 +171,8 @@ function injectCheckbox(HTMLEmbed) {
             }
         });
 
-        Clipboard.URICheckbox.addEventListener('click', () => {
-            if (Clipboard.URICheckbox.checked) {
+        URICheckbox.addEventListener('click', () => {
+            if (URICheckbox.checked) {
                 chrome.storage.local.set({
                     URI_STATE: true,
                 });
@@ -69,8 +183,8 @@ function injectCheckbox(HTMLEmbed) {
             }
         });
 
-        Clipboard.MDCheckbox.addEventListener('click', () => {
-            if (Clipboard.MDCheckbox.checked) {
+        MDCheckbox.addEventListener('click', () => {
+            if (MDCheckbox.checked) {
                 chrome.storage.local.set({
                     MD_STATE: true,
                 });
@@ -82,18 +196,21 @@ function injectCheckbox(HTMLEmbed) {
         });
 
         // Load the correct state of the checkmark based on local config data
-        Clipboard.configure();
+        configure();
 
         // Initiate the clibpoard modifications on embed load, on clicking into the embed,
         // and on refocusing the window (helpful if the user clicks away to Illustrator then clicks back in)
         // and on ctrl keyup (i.e. for ctrl+a when selecting everything inside the embed modal)
         modifyClipboard();
+
         HTMLEmbed.addEventListener('click', modifyClipboard);
+
         window.addEventListener('focus', () => {
             if (get('svg-checkbox')) {
                 modifyClipboard;
             }
         });
+
         document.addEventListener('keyup', (e) => {
             if (e.key === 'Control' && get('svg-checkbox')) {
                 modifyClipboard;
@@ -103,115 +220,4 @@ function injectCheckbox(HTMLEmbed) {
 
     // Start wait loop again
     lookFor('.bem-EmbedEditor_Modal', 100).then(injectCheckbox);
-}
-
-async function configure() {
-    const configData = await chrome.storage.local.get([
-        'SVG_STATE',
-        'MD_STATE',
-        'URI_STATE',
-    ]);
-    if (configData.SVG_STATE) {
-        this.SVGCheckbox.checked = true;
-    }
-    if (configData.MD_STATE) {
-        this.MDCheckbox.checked = true;
-    }
-    if (configData.URI_STATE) {
-        this.URICheckbox.checked = true;
-    }
-}
-
-async function modifyClipboard() {
-    const text = await navigator.clipboard.readText(),
-        configData = await chrome.storage.local.get([
-            'SVG_STATE',
-            'MD_STATE',
-            'URI_STATE',
-        ]);
-    if (SVGPragma.test(text)) {
-        // Checking config data to see if SVG compression is on
-        if (configData.SVG_STATE) {
-            // If so, compress and load that text to the clipboard
-            const compressedSVG = compressSVG(text);
-            navigator.clipboard.writeText(compressedSVG);
-        } else if (configData.URI_STATE) {
-            const convertedSVG = convertSVG(text);
-            navigator.clipboard.writeText(convertedSVG);
-        }
-    }
-    if (MDPragma.test(text)) {
-        if (configData.MD_STATE) {
-            const convertedMD = markdown(text.replace(MDPragma, ''));
-            navigator.clipboard.writeText(convertedMD);
-        }
-    }
-}
-
-// This is a severe minification, removes everything but the essential path data including colors, kerning, strokes and stroke effects
-// Best for small icons or simple shapes (which are the best use-cases for inline SVG in Webflow)
-
-function convertSVG(svg) {
-    svg = svg.trim();
-    // remove xml, doctype, generator...
-    svg = svg.slice(svg.indexOf('<svg'));
-    // soft validate
-    if (!svg.startsWith('<svg') || !svg.endsWith('svg>')) return;
-    // add namespace if necessary
-    if (!svg.includes('http://www.w3.org/2000/svg'))
-        svg = svg.replace(/<svg/g, `<svg xmlns='http://www.w3.org/2000/svg'`);
-    // remove comments
-    svg = svg.replace(/<!--.{1,}-->/g, '');
-    // remove unnecessary attributes
-    svg = svg.replace(/version=[\"\'](.{0,}?)[\"\'](?=[\s>])/g, '');
-    svg = svg.replace(/id=[\"\'](.{0,}?)[\"\'](?=[\s>])/g, '');
-    svg = svg.replace(/class=[\"\'](.{0,}?)[\"\'](?=[\s>])/g, '');
-    svg = svg.replace(/data-name=[\"\'](.{0,}?)[\"\'](?=[\s>])/g, '');
-    svg = svg.replace(/encoding=[\"\'](.{0,}?)[\"\'](?=[\s>])/g, '');
-    // replace nested quotes
-    svg = svg.replace(/"'(.{1,})'"/g, "'$1'");
-    // replace double quotes
-    svg = svg.replace(/"/g, "'");
-    // remove empty spaces between tags
-    svg = svg.replace(/>\s{1,}</g, '><');
-    // remove duplicate spaces
-    svg = svg.replace(/\s{2,}/g, ' ');
-    // trim again
-    svg = svg.trim();
-    // soft validate again
-    if (!svg.startsWith('<svg') || !svg.endsWith('svg>')) return;
-    // replace ampersand
-    svg = svg.replace(/&/g, '&amp;');
-    // encode only unsafe symbols
-    svg = svg.replace(/[%#<>?\[\\\]^`{|}]/g, encodeURIComponent);
-    // build data uri
-    svg = `data:image/svg+xml,${svg}`;
-    // ok, ship it!
-    return svg;
-}
-
-// This is a severe minification, removes everything but the essential path data including colors, kerning, strokes and stroke effects
-// Best for small icons or simple shapes (which are the best use-cases for inline SVG in Webflow)
-
-function compressSVG(svg) {
-    svg = svg.trim();
-    // remove xml, doctype, generator...
-    svg = svg.slice(svg.indexOf('<svg'));
-    // soft validate
-    if (!svg.startsWith('<svg') || !svg.endsWith('svg>')) return;
-    // add namespace if necessary
-    if (!svg.includes('http://www.w3.org/2000/svg'))
-        svg = svg.replace(/<svg/g, `<svg xmlns='http://www.w3.org/2000/svg'`);
-    // remove comments
-    svg = svg.replace(/<!--.{1,}-->/g, '');
-    // remove unnecessary attributes
-    svg = svg.replace(/version=[\"\'](.{0,}?)[\"\'](?=[\s>])/g, '');
-    svg = svg.replace(/id=[\"\'](.{0,}?)[\"\'](?=[\s>])/g, '');
-    svg = svg.replace(/class=[\"\'](.{0,}?)[\"\'](?=[\s>])/g, '');
-    svg = svg.replace(/data-name=[\"\'](.{0,}?)[\"\'](?=[\s>])/g, '');
-    svg = svg.replace(/encoding=[\"\'](.{0,}?)[\"\'](?=[\s>])/g, '');
-    svg = svg.replace(/style=[\"\'](.{0,}?)[\"\'](?=[\s>])/g, '');
-    svg = svg.replace(/fill=[\"\'](.{0,}?)[\"\'](?=[\s>])/g, '');
-
-    return svg;
 }
